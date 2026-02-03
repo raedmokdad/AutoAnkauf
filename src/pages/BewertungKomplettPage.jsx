257becbcbe3bb882caa5bc371e83d3be496274fa
Raed Mokdad <raedmokdad@yahoo.de>
2026-02-03 13:49:31 +0100

    Remove unused Link import

--- src/pages/BewertungKomplettPage.jsx
+++ src/pages/BewertungKomplettPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import SEO from '../components/SEO';
 import VehicleForm from '../components/VehicleForm';
 import { CheckIcon } from '../components/Icons';
